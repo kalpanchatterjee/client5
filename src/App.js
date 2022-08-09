691370f6da039043250ce671f94444193cc7feb8
kalpan.chattopadhyay <kalpan.chattopadhyay@rakuten.com>
2022-08-09 22:05:08 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,46 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useEffect, useState} from "react"
+import {useParams
+} from "react-router-dom";
 function App() {
+  let { id } = useParams();
+  const [token,setToken]=useState("");
+  useEffect(()=>{
+    //   var xhr = new XMLHttpRequest();
+    //   xhr.open('GET', 'http://localhost:3000/cookie', true);
+    //  // xhr.withCredentials = true;
+    //   xhr.send(null);
+    //   if (xhr.status == 200 && xhr.status < 300) {
+    //     // Runs when the request is successful
+    //     console.log(xhr);
+    //   } else {
+    //     // Runs when it's not
+    //   }
+    //const params = new URLSearchParams(window.location.pathname);
+    //console.log(params.token)
+   // alert("here"+id)
+    if(typeof(id)!='undefined' && id!=null && id!=""){
+      //alert("id"+id);
+      setToken(id);
+    }else{
+      //alert("here");
+      redirect("https://login-three-gamma.vercel.app/cookie/client4.vercel.app");
+    }
+      // axios.get("http://localhost:3000").then(res=>{
+      //   console.log(res.headers);
+      // })
+       
+      
+    },[])
+    const redirect =url=>{
+      console.log(url);
+      window.location.replace(url);
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      {token}
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Switch,
+} from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+  <Switch>
+          <Route path="/" exact children={<App />} />
+          <Route path="/:id" exact children={<App />} />
+          
+   </Switch>
+</BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
